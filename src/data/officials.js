06275567586bccd5ffd05bf3b7217fd61a6aeb56
Severi Casserly <severi@casserly.fi>
2022-09-18 12:41:53 +0300

    Extract officials data

---
+++ src/data/officials.js
@@ -0,0 +1,16 @@
+export const officialsData = [
+    {
+        name: "Fuksitoimikunta",
+        members: [
+            { name: "Etunimi Sukunimi", title: "Fuksikapteeni", leader: true },
+            { name: "Etunimi Sukunimi", title: "Fuksikapteeni", leader: true },
+            { name: "Etunimi Sukunimi", title: "Tosi ISOpomo" },
+            { name: "Etunimi Sukunimi", title: "SuurFuksikapteeni" },
+            { name: "Pitkäetunimi Pitkäsukunimi", title: "Tosi-ISO" },
+            { name: "Etunimi", title: "Senior Master's and International Student Captain" },
+            { name: "Etunimi", title: "Tosi-ISO" },
+            { name: "Etunimi", title: "Tosi-ISO" },
+            { name: "Etunimi", title: "Tosi-ISO" },
+        ]
+    }
+]
