75cb92a1378cb782b36dcc6db5e5569be38ddbec
Severi Casserly <severi@casserly.fi>
2022-10-27 13:55:05 +0300

    SnapWidget instagram feed embed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,16 @@ import type { HeadFC, HeadProps, PageProps } from "gatsby"
 import Meta from "@src/components/common/meta/meta"
 import CustomLayout from "@src/components/layouts/custom-layout.jsx"
 import Hero from "@src/components/common/hero/hero"
+import SnapWidget from "@src/components/instagram/SnapWidget"
+
 
 const IndexPage: React.FC<PageProps> = ({ pageContext }) => {
     return (
         <CustomLayout pageContext={pageContext}>
             <Hero title="Automaatio- ja systeemitekniikan kilta" />
             <p>Lorem ipsum dolor sit amet.</p>
+
+            <SnapWidget />
         </CustomLayout>
     )
 }
