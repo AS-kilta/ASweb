83db7ba48c21a9c16f901b8b6e60942823b50ea6
Severi Casserly <severi@casserly.fi>
2022-10-27 19:47:52 +0300

    EventList proto

---
+++ src/components/events/EventList.tsx
@@ -0,0 +1,83 @@
+import React, {useState, useEffect} from "react"
+import * as style from "./EventList.module.scss"
+
+const getStartDate = () => (new Date()).toISOString()
+
+const getEndDate = () => {
+    const date = new Date()
+    date.setMonth(date.getMonth() + 1);
+    return date.toISOString();
+}
+
+const getEvents = async (number: number) => {
+    const startDate = getStartDate();
+    const endDate = getEndDate();
+
+    const res = await fetch(`https://www.googleapis.com/calendar/v3/calendars/as.tiedottaja@gmail.com/events?key=AIzaSyCJrtmGOeEFAq912lwijvCmKR33SAtC_qo&singleEvents=true&orderBy=starttime&timeMin=${startDate}&timeMax=${endDate}`)
+    const data = await res.json();
+
+    return parseEventData(data);
+}
+
+interface CalendarEvent {
+    id: string;
+    summary: string;
+    start: Date;
+    end: Date;
+    description?: string;
+    location?: string;
+    fullDay: boolean;
+}
+
+const parseEventData = (data: any) => {
+    if (!data.items || !Array.isArray(data.items)) {
+        throw new Error("Invalid calendar data");
+    }
+
+    const filteredEventData = data.items.filter(item => item.organizer.self).slice(0, 5);
+    const events: CalendarEvent[] = filteredEventData.map(item => {
+        const start = new Date(item.start.date ?? item.start.dateTime);
+        const end = new Date(item.end.date ?? item.end.dateTime);
+        const fullDay = item.start.date ? true : false;
+        const { id, summary, description, location } = item;
+        return { id, summary, start, end, description, location, fullDay }
+    });
+
+    return events;
+}
+
+const dateOptions: Intl.DateTimeFormatOptions = { timeZone: "Europe/Helsinki", weekday: "short", year: "numeric", month: "numeric", day: "numeric" };
+const timeOptions: Intl.DateTimeFormatOptions = { hour: "2-digit", minute: "2-digit" }
+
+const EventList: React.FC<{number: number, lang: string}> = ({ number=5, lang="fi" })=> {
+    const [events, setEvents] = useState<CalendarEvent[]>([])
+
+    useEffect(() => {
+        getEvents(number)
+            .then((events) => {
+                setEvents(events)
+            })
+            .catch(error => {console.error(error)})
+    }, [])
+
+    return (
+        <ul className={style.event_list}>
+            {events.map(event => {
+                const datetimeOptions = event.fullDay
+                    ? dateOptions : { ...dateOptions, ...timeOptions};
+                return (
+                    <li>
+                        <div className={style.date}>{event.start.getDate()}.{event.start.getMonth()}.</div>
+                        <div>
+                            <div className={style.summary}>{event.summary}</div>
+                            <time>{event.start.toLocaleString(lang, datetimeOptions)}</time>
+                            <p>{event.description}</p>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default EventList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,14 @@ import Meta from "@src/components/common/meta/meta"
 import CustomLayout from "@src/components/layouts/custom-layout.jsx"
 import Hero from "@src/components/common/hero/hero"
 import SnapWidget from "@src/components/instagram/SnapWidget"
-
+import EventList from "@src/components/events/EventList"
 
 const IndexPage: React.FC<PageProps> = ({ pageContext }) => {
     return (
         <CustomLayout pageContext={pageContext}>
             <Hero title="Automaatio- ja systeemitekniikan kilta" />
             <p>Lorem ipsum dolor sit amet.</p>
-
+            <EventList number={5} />
             <SnapWidget />
         </CustomLayout>
     )
