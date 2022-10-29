2a183906cbc320da7a4b7e11fd00b87c5e7ca087
Severi Casserly <severi@casserly.fi>
2022-10-29 12:41:57 +0300

    PageProps

--- src/pages/en/guild/board.tsx
+++ src/pages/en/guild/board.tsx
@@ -1,14 +1,11 @@
 import React from "react"
-import type { HeadFC, HeadProps, PageProps } from "gatsby"
+import type { HeadFC, HeadProps } from "gatsby"
 import Board from "@src/components/profiles/Board"
 import Meta from "@src/components/common/meta/meta"
 import PageLayout from "@src/components/layouts/page-layout.jsx"
 
-interface Props extends PageProps {
-  pageContext: any
-}
 
-const BoardPage: React.FC<Props> = ({ pageContext }) => {
+const BoardPage: React.FC<PageProps> = ({ pageContext }) => {
   return (
     <PageLayout pageContext={pageContext} title="Board">
       <Board lang={pageContext.lang} />
