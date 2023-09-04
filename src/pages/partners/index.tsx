439d7170d34220cc090980208ed575bb6b4f6e0d
Miska-Mynttinen <miska.myntt@gmail.com>
2023-09-04 23:35:44 +0300

    improved yrityksille css

--- src/pages/partners/index.tsx
+++ src/pages/partners/index.tsx
@@ -17,35 +17,41 @@ const Partners: React.FC<PageProps> = ({ pageContext }) => {
 
   return (
     <PageLayout pageContext={pageContext} background="https://as.fi/static/abimarkkinointi/Antti_Regelin.jpg" heroHeight="60" title="Yrityksille">
-      <div style={{marginBlock: "6rem"}}>
-        <strong>Automaatio- ja systeemitekniikan kilta on ainejärjestö Aalto-yliopiston Sähkötekniikan korkeakoulun Automaatio- ja informaatioteknologian opiskelijoille. Kilta tarjoaa jäsenilleen ympäri vuoden monenlaista ajanvietettä paitsi opinnoista rentoutumiseen, myös omien tietojensa ja taitojensa kartuttamiseen erilaisten harrastetoimintojen parissa.</strong>
-        <p>Kiltalaisistamme kasvaa opintovuosien saatossa uusia osaajia niin automaation, robotiikan, älykkäiden laitteiden kuin myös tietoverkkojen, koneoppimisen ja signaalinkäsittelyn pariin. Useimmat työllistyvät alalleen konsulteiksi, ohjelmistokehittäjiksi tai vaativiin tutkimus- ja kehitystehtäviin.</p>
-        <p>Nuoret osaajat ovat kovaa valuuttaa työmarkkinoilla ja monet meistä pyrkivätkin hankkimaan alan työkokemusta jo opiskeluaikoina. Kiltamme kautta on mahdollista tavoittaa tehokkaasti huomattava osa tulevaisuuden ammattilaisista jo varhaisessa vaiheessa. Monet yritykset ovatkin ryhtyneet yhteistyökumppaneiksemme saadakseen lisää näkyvyyttä ja tukeneet samalla kiltamme toimintaa. Alla onkin muutamia yleisimpiä yhteistyötapoja.</p>
+      <div className={style.introduction}>
+        <div className={style.testimonials}>
+          <ContactCard data={lucasData}/>
+        </div>
+        <p className={style.intro_quote}>"Hei yrityksen edustaja! Olen Automaatio- ja systeemitekniikan killan yrityssuhdevastaava. Kantsii tehä meidän kanssa yhteistyötä ja tältä sivulta löydät lisätietoa yhteistyömahdollisuuksista. Ota minuun yhteyttä niin jutellaan lisää ja räätälöidään just teille sopiva paketti!"</p>
       </div>
-      
-      <section className={style.major}>
-        <h3>Excursiot</h3>
+      <div className={style.title_container}><h3 className={style.top_title}>Yhteistyö killan kanssa</h3></div>
+        <div>
+          <strong>Automaatio- ja systeemitekniikan kilta on ainejärjestö Aalto-yliopiston Sähkötekniikan korkeakoulun Automaatio- ja informaatioteknologian opiskelijoille. Kilta tarjoaa jäsenilleen ympäri vuoden monenlaista ajanvietettä paitsi opinnoista rentoutumiseen, myös omien tietojensa ja taitojensa kartuttamiseen erilaisten harrastetoimintojen parissa.</strong>
+          <p className={style.text_box}>Kiltalaisistamme kasvaa opintovuosien saatossa uusia osaajia niin automaation, robotiikan, älykkäiden laitteiden kuin myös tietoverkkojen, koneoppimisen ja signaalinkäsittelyn pariin. Useimmat työllistyvät alalleen konsulteiksi, ohjelmistokehittäjiksi tai vaativiin tutkimus- ja kehitystehtäviin.</p>
+          <p className={style.text_box}>Nuoret osaajat ovat kovaa valuuttaa työmarkkinoilla ja monet meistä pyrkivätkin hankkimaan alan työkokemusta jo opiskeluaikoina. Kiltamme kautta on mahdollista tavoittaa tehokkaasti huomattava osa tulevaisuuden ammattilaisista jo varhaisessa vaiheessa. Monet yritykset ovatkin ryhtyneet yhteistyökumppaneiksemme saadakseen lisää näkyvyyttä ja tukeneet samalla kiltamme toimintaa. Alla onkin muutamia yleisimpiä yhteistyötapoja.</p>
+        </div>
+      <section className={style.products}>
+        <div className={style.title_container}><h3>Excursiot</h3></div>
         <div className={style.columns}>
           <div className={style.intro}>
-            <p>
+            <p className={style.text_box}>
               <strong>Excursioilla </strong> 
               yritykset pääsevät kertomaan itsestään ja esittelemään toimintaansa omalla toimipisteellään. Matkaan lähtee joukko motivoituneita opiskelijoita, jotka haluavat tutustua potentiaaliseen työnantajaansa iloisissa merkeissä. Monesti excursioihin on liittynyt jokin toiminnallinen osuus esittelyn ohella, jolloin kokemuksesta tulee mieleenpainuvampi.
             </p>
           </div>
         </div>
-        <h3>Haalarit</h3>
+        <div className={style.title_container}><h3>Haalarit</h3></div>
         <div className={style.columns}>
           <div className={style.intro}>
-            <p>
+            <p className={style.text_box}>
               <strong>Haalareista </strong>
               tunnistaa opiskelijan. Killassamme aloittaa joka vuosi yli 100 uutta opiskelijaa ja määrä on vuosi vuodelta ollut kasvussa. Uudet opiskelijat saavat itselleen ensimmäisen syksyn aikana opiskelijahaalarit, joissa on yhteistyökumppaniemme logoja painettuina. Logo haalareissa takaa näkyvyyden aina siellä, missä haalaripukeiset kiltalaiset ikinä kulkevatkaan.
             </p>
           </div>
         </div>
-        <h3>Rekrytointinäkyvyys</h3>
+        <div className={style.title_container}><h3>Rekrytointinäkyvyys</h3></div>
         <div className={style.columns}>
           <div className={style.intro}>
-            <p>
+            <p className={style.text_box}>
               Jaamme mielellämme meitä tukeneiden yritysten rekrytointi-ilmoituksia medioissamme. 
               <strong> Rekrytointilistallamme </strong> 
               työpaikkailmoituksenne tavoittavat kaikki kiltamme jäsenet. Julkaisemme myös neljä kertaa vuodessa omaa
@@ -61,10 +67,10 @@ const Partners: React.FC<PageProps> = ({ pageContext }) => {
             </p>
           </div>
         </div>
-        <h3>…ja myös paljon muuta!</h3>
+        <div className={style.title_container}><h3>…ja myös paljon muuta!</h3></div>
         <div className={style.columns}>
           <div className={style.intro}>
-            <p>
+            <p className={style.text_box}>
               Koko kiltatoiminta on monen pienen osan summa. Ylläpidämme yliopiston tiloissa kiltahuonettamme ASkia, joka toimii kiltalaisten olohuoneena luentojen ja muun koulunkäynnin lomassa. Järjestämme vuoden mittaan lukuisia eri tapahtumia jäsenillemme pienistä saunailloista aina arvokkaaseen vuosijuhlaamme Stimulaatioon asti. Tukemalla esimerkiksi tarvehankintojamme tai tapahtumiamme voi saada näissä lisää näkyvyyttä itselleen. Viime vuosina esimerkiksi Stimulaatioon on osallistunut kumppaniyritystemme edustajia mukaan juhliin!
             </p>
           </div>
@@ -72,15 +78,12 @@ const Partners: React.FC<PageProps> = ({ pageContext }) => {
         <h2>Kiinnostuitko? Ota yhteyttä!</h2>
         <div className={style.columns}>
           <div className={style.intro}>
-            <p>
+            <p className={style.text_box}>
               Yritysyhteistyöstä vastaa meidän päässämme yrityssuhdetoimikunta, jota koordinoi yrityssuhdevastaava.
             </p>
           </div>
         </div>
       </section>
-      <div className={style.testimonials}>
-        <ContactCard data={lucasData}/>
-      </div>
     </PageLayout>
   )
 }
