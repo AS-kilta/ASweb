33a7c06ccf40410fe90021cc5f72a93d56ac31e9
Erke Elias <elias.erke@aalto.fi>
2023-09-14 18:13:44 +0300

    s

--- src/pages/fuksis/fuksiguide.tsx
+++ src/pages/fuksis/fuksiguide.tsx
@@ -2,18 +2,20 @@ import React from "react"
 import type { HeadFC, HeadProps } from "gatsby"
 import Meta from "@src/components/common/Meta"
 import * as style from "./fuksis.module.scss"
-import SeparatedHeading1 from "@src/components/common/SeparatedHeading1"
-import CustomLayout from "@src/components/layouts/CustomLayout"
+import PageLayout from "@src/components/layouts/PageLayout"
 
 const Fuksiguide: React.FC<PageProps> = ({ pageContext }) => {
   return (
-    <CustomLayout pageContext={pageContext}>
-        <div className={style.board}>
-            <SeparatedHeading1 id='fuksiopas'> Fuksiopas 2023 </SeparatedHeading1>
-        </div>
-        <div className={style.text}>Fuksiopas sisältää kaiken, mitä sinun tarvitsee tietää tulevasta opiskelijaelämästä ja AS-killasta.</div>
+    <PageLayout pageContext={pageContext} title="Fuksiopas 2023">
+      <div className={style.text}>Fuksiopas sisältää kaiken, mitä sinun tarvitsee tietää tulevasta opiskelijaelämästä ja AS-killasta.</div>
+      
+      <a className={style.rounded + " rounded-button"} target="_blank" href="https://drive.google.com/file/d/14Ew0gsN5HKbIRWwXVne50ICVLe5d35TC/view?usp=sharing">Lataa PDF</a>
 
-    </CustomLayout>
+      
+      <div>
+        <iframe src="https://drive.google.com/file/d/14Ew0gsN5HKbIRWwXVne50ICVLe5d35TC/preview" width="640" height="480" allow="autoplay"></iframe>
+      </div>
+    </PageLayout>
   )
 }
 
