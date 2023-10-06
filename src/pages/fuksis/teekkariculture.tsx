eaeb476e7749eb23fad60a8c8c5f4113a0849507
Erke Elias <elias.erke@aalto.fi>
2023-10-06 17:36:54 +0300

    css fix

--- src/pages/fuksis/teekkariculture.tsx
+++ src/pages/fuksis/teekkariculture.tsx
@@ -9,7 +9,7 @@ const Teekkariculture: React.FC<PageProps> = ({ pageContext }) => {
   return (
     <PageLayout pageContext={pageContext} title='Teekkarikulttuuri' background='https://as.fi/static/abimarkkinointi/Antti_Regelin.jpg' heroHeight='60'>
 
-        <SeparatedHeading1 id='haalarit'>Haalarit</SeparatedHeading1>
+        <h1 id='haalarit' className={style.heading1}>Haalarit</h1>
 
         <div className={style.text}>
           <p>Haalarit ovat asuste, josta teekkarin tunnistaa. Haalareita käytetään monissa tilaisuuksissa, joissa liikutaan julkisilla paikoilla teekkareina. Jokaisella killalla on omanvärisensä haalarit. Ne ovat toisin sanoen teekkarin univormu.</p>
@@ -18,7 +18,7 @@ const Teekkariculture: React.FC<PageProps> = ({ pageContext }) => {
           <p>Muista, että haalareita ei saa pestä! Jokainen tahra ja naarmu kertoo kokemuksista. Uudet fuksit saavat AS:n kauniin violetit haalarit Haalarigaalassa, joka järjestetään syyskuun lopulla.</p>
         </div>
 
-        <SeparatedHeading1 id='tilaaminen'> Tilaaminen </SeparatedHeading1>
+        <h2 id='tilaaminen' className={style.heading2}> Tilaaminen </h2>
 
         <div className={style.text}>
           <p>Haalareita voi tilata orientaatioviikolla. Lisätilaukset tulevat kalliiksi, joten jos aiot tilata, se kannattaa tehdä heti! Tilaus on sitova.</p>
@@ -26,19 +26,19 @@ const Teekkariculture: React.FC<PageProps> = ({ pageContext }) => {
           <p>Jos havaitset haalareissa jotain vikaa tai valitettavaa, niin ilmoitathan asiasta heti. Valmistusvirheistä johtuvat viat korjataan kyllä. Kaikki osat, kuten napit ja vetoketju, eivät välttämättä kestä kovaa menoa, mutta ne on sangen helppo korjailla itse. Kysy vinkkejä vanhemmilta killisteiltä.</p>
         </div>
 
-        <SeparatedHeading1 id='teekkarilakki'> Teekkarilakki </SeparatedHeading1>
+        <h1 id='teekkarilakki' className={style.heading1}> Teekkarilakki </h1>
 
         <div className={style.text}>
           <p>Teekkarilakki on teekkareiden tunnusmerkki, jonka saa painaa päähänsä ensimmäisenä Wappunaan. Nykyisen mallinen lakki on keikkunut teekkareiden päässä jo yli sadan vuoden ajan. Päällisin puolin se muistuttaa tavallista ylioppilaslakkia, mutta sen kuusi kulmaa, musta tuuhea tupsu ja hammasrataskokardi viestivät kaukaa kantajansa arvokkuudesta.</p>
         </div>
 
-        <SeparatedHeading1 id='lakin-kaytto'> Lakin käyttö </SeparatedHeading1>
+        <h2 id='lakin-kaytto' className={style.heading2}> Lakin käyttö </h2>
 
         <div className={style.text}>
           <p>Teekkarilakki on teekkareiden tunnusmerkki, jonka saa painaa päähänsä ensimmäisenä Wappunaan. Nykyisen mallinen lakki on keikkunut teekkareiden päässä jo yli sadan vuoden ajan. Päällisin puolin se muistuttaa tavallista ylioppilaslakkia, mutta sen kuusi kulmaa, musta tuuhea tupsu ja hammasrataskokardi viestivät kaukaa kantajansa arvokkuudesta.</p>
         </div>
 
-        <SeparatedHeading1 id='oikeus-lakin-kayttoon'> Oikeus lakin käyttöön </SeparatedHeading1>
+        <h2 id='oikeus-lakin-kayttoon' className={style.heading2}> Oikeus lakin käyttöön </h2>
 
         <div className={style.text}>
           <p>Teekkarilakkia ei kuka tahansa saa käyttää - se on ansaittava. Jokainen ensimmäisenä vuotenaan kunnostautunut fuksi saa painaa lakin päähänsä mahdollisesti järjestettävänä Wappuna täsmälleen keskiyöllä. Syksyllä taas lakki lasketaan olalle yksissä vuoden suurimmissa bileissä, Lakinlaskijaisissa.</p>
