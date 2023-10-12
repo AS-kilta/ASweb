fb9167351d111d6c4abd05c41124027c6da28ab9
Severi Casserly <severi@casserly.fi>
2023-10-12 20:17:15 +0300

    Make layout width narrower

--- src/pages/fuksis/fuksiguide.tsx
+++ src/pages/fuksis/fuksiguide.tsx
@@ -6,12 +6,12 @@ import PageLayout from "@src/components/layouts/PageLayout"
 
 const Fuksiguide: React.FC<PageProps> = ({ pageContext }) => {
   return (
-    <PageLayout pageContext={pageContext} title="Fuksi 2023">
-      <div className={style.text}>Fuksiopas sisältää kaiken, mitä sinun tarvitsee tietää tulevasta opiskelijaelämästä ja AS-killasta.</div>
-
-      <a className={style.rounded + " rounded-button"} target="_blank" href="https://drive.google.com/file/d/14Ew0gsN5HKbIRWwXVne50ICVLe5d35TC/view?usp=sharing">Lataa PDF</a>
-
-      <div >
+    <PageLayout pageContext={pageContext} title="Fuksi 2023" lead="Fuksiopas sisältää kaiken, mitä sinun tarvitsee tietää tulevasta opiskelijaelämästä ja AS-killasta.">
+      <div style={{width: "100%"}}>
+        <a className={style.rounded + " rounded-button"}
+          target="_blank" href="https://drive.google.com/file/d/14Ew0gsN5HKbIRWwXVne50ICVLe5d35TC/view?usp=sharing">
+            Lataa PDF
+        </a>
         <iframe src="https://drive.google.com/file/d/14Ew0gsN5HKbIRWwXVne50ICVLe5d35TC/preview" width="100%" height="600" allow="autoplay"></iframe>
       </div>
     </PageLayout>
