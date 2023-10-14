db5cde90eab609aeb3db562f1ef3bdad06fa8d08
Severi Casserly <severi@casserly.fi>
2023-10-14 19:28:28 +0300

    Inherit hero props in pagelayout

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,18 +1,14 @@
 import React from "react"
 
 import Navbar from "@src/components/common/Navbar"
-import Hero from "@src/components/common/Hero"
+import Hero, { HeroProps } from "@src/components/common/Hero"
 import Footer from "@src/components/common/Footer"
 
 import * as style from "./PageLayout.module.scss"
 
-interface Props {
+interface Props extends HeroProps {
     pageContext: PageProps["pageContext"],
-    title: string,
-    lead?: string,
-    background?: string,
     children?: React.ReactNode,
-    heroHeight?: "short" | "tall"
 }
 
 const PageLayout: React.FC<Props> = ({ pageContext, title, lead, background, heroHeight, children }) => {
