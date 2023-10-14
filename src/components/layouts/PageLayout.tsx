73fd52c37906cce3bf8db96bcfc73e32aacb6474
ejerke <110688776+ejerke@users.noreply.github.com>
2023-10-14 16:10:24 +0300

    Finnish membership page (#62)
    * Finnish fuksi page

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -12,7 +12,7 @@ interface Props {
     lead?: string,
     background?: string,
     children?: React.ReactNode,
-    heroHeight?: string
+    heroHeight?: "short" | "tall"
 }
 
 const PageLayout: React.FC<Props> = ({ pageContext, title, lead, background, heroHeight, children }) => {
@@ -23,7 +23,7 @@ const PageLayout: React.FC<Props> = ({ pageContext, title, lead, background, her
                 slug={pageContext.slug}
                 translation={pageContext.translation}
             />
-            <Hero title={title} lead={lead} background={background} height={heroHeight} />
+            <Hero title={title} lead={lead} background={background} heroHeight={heroHeight} />
             <div id={style.content}>
                 {children}
             </div>
