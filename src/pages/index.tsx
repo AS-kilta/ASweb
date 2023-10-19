48652fcbe3d070b7c1b2bf47f343a11d96ea007c
Severi Casserly <severi@casserly.fi>
2023-10-19 19:40:24 +0300

    Fix frontpage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,34 @@
 import React from "react"
 import type { HeadFC, HeadProps } from "gatsby"
 import Meta from "@src/components/common/Meta"
-import CustomLayout from "@src/components/layouts/CustomLayout"
-import Hero from "@src/components/common/Hero"
+import PageLayout from "@src/components/layouts/PageLayout"
 import SnapWidget from "@src/components/instagram/SnapWidget"
 import EventList from "@src/components/events/EventList"
 import * as style from './index.module.scss'
 
 import heroimg from "../images/images/20201029195159-eef0d345-xx.jpg"
-import pohina1 from "../images/images/karkki_kalle.jpg"
-import festival from "../images/images/20210917174935-70c53c15-xx.jpg"
+import sweets from "../images/images/karkki_kalle.jpg"
+import amfi from "../images/images/20210917174935-70c53c15-xx.jpg"
 
 const IndexPage: React.FC<PageProps> = ({ pageContext }) => {
     return (
-        <CustomLayout pageContext={pageContext}>
-            <Hero title="Automaatio- ja systeemitekniikan kilta" background={heroimg}/>
+        <PageLayout pageContext={pageContext} title="Automaatio- ja systeemitekniikan kilta" background={heroimg} >
             <div className={`even-columns ${style.frontpagebox}`}>
                 <div style={{textAlign: "center"}}>
                     <h2>Mikä AS?</h2>
                     <p>AS on opiskelijajärjestö, joka yhdistää Aalto-yliopiston opiskelijoilta monelta eri alalta. Kiltaamme kuuluu Automaation ja robotiikan, Informaatioteknologian ja Digital Systems and Designin sekä niitä lähellä olevien tutkinto-ohjelmien opiskelijoita. Kilta järjestää jäsenilleen vapaa-ajan toimintaa, valvoo heidän etuaan opintoihin liittyvissä asioissa ja ylläpitää suhteita alan yrityksiin.</p>
                 </div>
-                <img src={festival} alt="" />
+                <img src={amfi} alt="" />
             </div>
             <div className={`even-columns ${style.frontpagebox}`}>
-                <img className="no-mobile" src={pohina1} alt="" />
+                <img className="no-mobile" src={sweets} alt="" />
                 <div className={style.eventsContainer}>
                     <h2>Tapahtumat</h2>
                     <EventList number={5} lang={pageContext.lang} />
                 </div>
             </div>
             <SnapWidget />
-        </CustomLayout>
+        </PageLayout>
     )
 }
 
