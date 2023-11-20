15b116be134bc0c9a71c86d9290ea48230f23ef2
Jyri Kolu <jyri.kolu@gmail.com>
2023-11-20 03:05:41 +0200

    Implementation for replaying and font sizing that works decently with the most of the common browsers

--- static/demo/demo.js
+++ static/demo/demo.js
@@ -174,7 +174,12 @@ function initialize() {
 
     const text = decodeURI(window.location.search.split("=")[1])
 
-    context.font = "bold clamp(2rem, 10vw, 7rem) 'Comic Sans MS', 'Comic Sans', 'Chalkboard SE', sans-serif";
+    // Dirty regex to detect Safari, as the font-sizing sucks on it...
+
+    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent))
+        context.font = "bold 10vw 'Comic Sans MS', 'Comic Sans', 'Chalkboard SE', sans-serif";
+    else
+        context.font = "bold clamp(2rem, 10vw, 8rem) 'Comic Sans MS', 'Comic Sans', 'Chalkboard SE', sans-serif";
     context.textAlign = "center";
     context.fillText(text, width / 2, height / 2);
 
