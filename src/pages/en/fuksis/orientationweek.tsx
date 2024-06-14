b57520d0f8c7a7ca492426c3c6532de4701e0db2
epa <huhtaee@gmail.com>
2024-06-14 18:02:00 +0300

    aloitettu fuksien o-viikon aikataulu sivun tekeminen

--- src/pages/en/fuksis/orientationweek.tsx
+++ src/pages/en/fuksis/orientationweek.tsx
@@ -3,6 +3,7 @@ import type { HeadFC, HeadProps } from "gatsby"
 import Meta from "@src/components/common/Meta"
 import PageLayout from "@src/components/layouts/PageLayout"
 //import heropic from "@src/images/images/IMG_6067-Edit.jpg" //pitää vaihtaa
+import { StaticImage } from "gatsby-plugin-image"
 
 
 const Orientaatioviikko: React.FC<PageProps> = ({ pageContext }) => {
@@ -14,11 +15,14 @@ const Orientaatioviikko: React.FC<PageProps> = ({ pageContext }) => {
       heroHeight='short'
     >
 
-        <h2 id='Orientaatioviikon aikataulu'>aikataulu</h2>
-
+        <h2 id='Orientaatioviikon aikataulu'></h2>
+        
         <div>
-          <p></p>
+          <p>Orientation week is a hectic week with lots to do and explore!</p>
+          <p>Here is the orientation week schedule as of now, we will try to update this to the best of our ability during summer</p>
+          <StaticImage className="image wide" src='../../images/images/DSD_timetable.jpg' alt=''/>
         </div>
+        
     </PageLayout>
   )
 }
