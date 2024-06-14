723e0b6ee818ae8bbec581208c559ad9ee1438ae
epa <huhtaee@gmail.com>
2024-06-14 19:28:32 +0300

    Englanninkielinen orientaation aikataulu on valmis, suomenkielinen on tulossa

--- src/pages/en/fuksis/orientationweek.tsx
+++ src/pages/en/fuksis/orientationweek.tsx
@@ -10,19 +10,25 @@ const Orientaatioviikko: React.FC<PageProps> = ({ pageContext }) => {
   return (
     <PageLayout
       pageContext={pageContext}
-      title='Orientaatioviikko'
+      title='Orientation week'
 //      background={heropic}
       heroHeight='short'
     >
 
         <h2 id='Orientaatioviikon aikataulu'></h2>
         
-        <div>
+        <div className="image-container">
           <p>Orientation week is a hectic week with lots to do and explore!</p>
-          <p>Here is the orientation week schedule as of now, we will try to update this to the best of our ability during summer</p>
-          <StaticImage className="image wide" src='../../images/images/DSD_timetable.jpg' alt=''/>
+          <p>Here is the orientation week schedule as of now, we will try to update this to the best of our ability during summer.</p>
+          <StaticImage className="centered-image" src='../../../images/images/DSD_timetable.jpg' alt='"DSD_timetable'/>
         </div>
-        
+        <div>
+          <p>If you have any questions about the schedule or starting your studies, don't be afraid to message us at fuksikapteenit@as.fi or on telegram @fuksikapteenit</p>
+
+          <p>Have a lovely summer!</p>
+          <p>Your Fuksi Captains: Eero and Jenny</p>
+          
+        </div>
     </PageLayout>
   )
 }
--- src/pages/fuksis/orientationweek.tsx
+++ src/pages/fuksis/orientationweek.tsx
@@ -3,6 +3,7 @@ import type { HeadFC, HeadProps } from "gatsby"
 import Meta from "@src/components/common/Meta"
 import PageLayout from "@src/components/layouts/PageLayout"
 //import heropic from "@src/images/images/IMG_6067-Edit.jpg" //pitää vaihtaa
+import { StaticImage } from "gatsby-plugin-image"
 
 
 const Orientaatioviikko: React.FC<PageProps> = ({ pageContext }) => {
@@ -14,10 +15,19 @@ const Orientaatioviikko: React.FC<PageProps> = ({ pageContext }) => {
       heroHeight='short'
     >
 
-        <h2 id='Orientaatioviikon aikataulu'>aikataulu</h2>
-
+        <h2 id='Orientaatioviikon aikataulu'></h2>
+        
+        <div className="image-container">
+          <p>Orientaatioviikko on hektistä aikaa, tekemistä on paljon!</p>
+          <p>Tässä orientaatioviikon aikataulu, päivittelemme tätä parhaamme mukaan kesän aikana.</p>
+          <StaticImage className="centered-image" src='../../images/images/DSD_timetable.jpg' alt='"DSD_timetable'/>
+        </div>
         <div>
-          <p></p>
+          <p>If you have any questions about the schedule or starting your studies, don't be afraid to message us at fuksikapteenit@as.fi or on telegram @fuksikapteenit</p>
+
+          <p>Nauttikaa kesästä!</p>
+          <p>Fuksikapteenisi: Eero ja Jenny</p>
+          
         </div>
     </PageLayout>
   )
