654fd2d328281bac20ba23605cdd039efe8df0e8
epa <huhtaee@gmail.com>
2024-07-04 19:00:18 +0300

    desc muutettu, ja "lataa pdf" -> "download pdf"

--- src/pages/en/fuksis/fuksiguide.tsx
+++ src/pages/en/fuksis/fuksiguide.tsx
@@ -9,7 +9,7 @@ const Fuksiguide: React.FC<PageProps> = ({ pageContext }) => {
       <div style={{width: "100%"}}>
         <a style={{marginBlock: "1rem"}} className="rounded-button"
             target="_blank" href="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/view?usp=sharing">
-              Lataa PDF
+              Download PDF
           </a>
           <iframe src="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/preview" width="100%" height="800" allow="autoplay"></iframe>
       </div>
@@ -20,7 +20,7 @@ const Fuksiguide: React.FC<PageProps> = ({ pageContext }) => {
 
 export default Fuksiguide
 
-const description: string = "Psst! This page is worth a visit towards the end of June."
+const description: string = "This guide contains everything you need to know about the university, studying and student life"
 
 export const Head: HeadFC<HeadProps> = () => (
   <Meta lang="en" title="Fuksiguide" description={description} />
