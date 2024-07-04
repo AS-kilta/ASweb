8146139fc1c7a65808481896a1bd0d5ab50d72dd
epa <huhtaee@gmail.com>
2024-07-04 18:08:41 +0300

    lisätty dsd fopas ja muutettu pari otsikkoa

--- src/pages/en/fuksis/fuksiguide.tsx
+++ src/pages/en/fuksis/fuksiguide.tsx
@@ -5,8 +5,13 @@ import PageLayout from "@src/components/layouts/PageLayout"
 
 const Fuksiguide: React.FC<PageProps> = ({ pageContext }) => {
   return (
-    <PageLayout pageContext={pageContext} title="Fuksiguide 2024" lead="Psst! This page is worth a visit towards the end of June">
+    <PageLayout pageContext={pageContext} title="DSD Fuksiguide 2024" lead="This guide contains everything you need to know about the university, studying and student life">
       <div style={{width: "100%"}}>
+        <a style={{marginBlock: "1rem"}} className="rounded-button"
+            target="_blank" href="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/view?usp=sharing">
+              Lataa PDF
+          </a>
+          <iframe src="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/preview" width="100%" height="800" allow="autoplay"></iframe>
       </div>
     </PageLayout>
   )
