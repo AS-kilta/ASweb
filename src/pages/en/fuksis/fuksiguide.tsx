4e304dc691e6f5a353723adee7ec94302fef1910
Akseli Pihlajamaa <akselijuhanipihlajamaa@gmail.com>
2024-07-05 16:57:41 +0300

    format tsx files

--- src/pages/en/fuksis/fuksiguide.tsx
+++ src/pages/en/fuksis/fuksiguide.tsx
@@ -1,27 +1,38 @@
-import React from "react"
-import type { HeadFC, HeadProps } from "gatsby"
-import Meta from "@src/components/common/Meta"
-import PageLayout from "@src/components/layouts/PageLayout"
+import React from 'react';
+import type { HeadFC, HeadProps } from 'gatsby';
+import Meta from '@src/components/common/Meta';
+import PageLayout from '@src/components/layouts/PageLayout';
 
 const Fuksiguide: React.FC<PageProps> = ({ pageContext }) => {
   return (
-    <PageLayout pageContext={pageContext} title="DSD Fuksiguide 2024" lead="This guide contains everything you need to know about the university, studying and student life">
-      <div style={{width: "100%"}}>
-        <a style={{marginBlock: "1rem"}} className="rounded-button"
-            target="_blank" href="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/view?usp=sharing">
-              Download PDF
-          </a>
-          <iframe src="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/preview" width="100%" height="800" allow="autoplay"></iframe>
+    <PageLayout
+      pageContext={pageContext}
+      title="DSD Fuksiguide 2024"
+      lead="This guide contains everything you need to know about the university, studying and student life"
+    >
+      <div style={{ width: '100%' }}>
+        <a
+          style={{ marginBlock: '1rem' }}
+          className="rounded-button"
+          target="_blank"
+          href="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/view?usp=sharing"
+        >
+          Download PDF
+        </a>
+        <iframe
+          src="https://drive.google.com/file/d/1eldNSVX6_pCUnbdpfvQYKRjFwWI-gZ-B/preview"
+          width="100%"
+          height="800"
+          allow="autoplay"
+        ></iframe>
       </div>
     </PageLayout>
-  )
-}
+  );
+};
 
+export default Fuksiguide;
 
-export default Fuksiguide
+const description: string =
+  'This guide contains everything you need to know about the university, studying and student life';
 
-const description: string = "This guide contains everything you need to know about the university, studying and student life"
-
-export const Head: HeadFC<HeadProps> = () => (
-  <Meta lang="en" title="Fuksiguide" description={description} />
-)
+export const Head: HeadFC<HeadProps> = () => <Meta lang="en" title="Fuksiguide" description={description} />;
