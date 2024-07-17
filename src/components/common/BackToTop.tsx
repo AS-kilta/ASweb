73092962dd9db3ecd6f854a5f1e24fa1d20a104c
Jyri Kolu <jyri.kolu@gmail.com>
2024-07-17 09:37:24 +0300

    Add "back to top"-button to page layout

---
+++ src/components/common/BackToTop.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { BsArrowUpShort } from 'react-icons/bs';
+import * as style from './BackToTop.module.scss';
+
+const BackToTop: React.FC<{ lang: string }> = ({ lang }) => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const scrollToTop: () => void = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  useEffect(() => {
+    const handleVisibility: () => void = () => {
+      const scrollPosition: number = window.scrollY;
+      if (scrollPosition >= 300 && !visible) setVisible(true);
+      else if (scrollPosition < 300 && visible) setVisible(false);
+    };
+    window.addEventListener('scroll', handleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', handleVisibility);
+    };
+  }, [visible]);
+
+  const classes: string = `${style.button} ${visible ? style.visible : ''}`;
+  const translations: Translations = {
+    backToTop: {
+      fi: 'Takaisin ylös',
+      en: 'Back to top',
+    },
+  };
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={classes}
+      title={translations.backToTop[lang]}
+      aria-label={translations.backToTop[lang]}
+    >
+      <BsArrowUpShort />
+    </button>
+  );
+};
+
+export default BackToTop;
