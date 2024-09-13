97f7f09b7fee1f41ef3d9286eb317737ad15587f
Ileska <ileska@tutanota.com>
2024-09-13 12:55:49 +0300

    Change scss path

--- src/pages/en/studies/advocacy.tsx
+++ src/pages/en/studies/advocacy.tsx
@@ -1,168 +1,106 @@
-import React from "react"
-import type { HeadFC, HeadProps } from "gatsby"
-import Meta from "@src/components/common/Meta"
-import * as style from "./studies.module.scss"
-import PageLayout from "@src/components/layouts/PageLayout"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import type { HeadFC, HeadProps } from 'gatsby';
+import Meta from '@src/components/common/Meta';
+import * as style from './../../studies/studies.module.scss';
+import PageLayout from '@src/components/layouts/PageLayout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 interface Props extends PageProps {
-  data: ImageArrayProps
+  data: ImageArrayProps;
 }
 
 const Advocacy: React.FC<Props> = ({ pageContext }) => {
   return (
     <PageLayout pageContext={pageContext} title="Advocacy">
       <p>
-        At Aalto, we believe in the power of advocacy to drive positive change
-        and create a more inclusive and welcoming environment for all.
-        Therefore, it is essential for everyone to be aware of their rights and
-        surroundings, ensuring that their voices are not only heard but also
-        respected and acted upon.
+        At Aalto, we believe in the power of advocacy to drive positive change and create a more inclusive and welcoming
+        environment for all. Therefore, it is essential for everyone to be aware of their rights and surroundings,
+        ensuring that their voices are not only heard but also respected and acted upon.
       </p>
       <h2>Halloped</h2>
       <p>
-        Hallopeds are students chosen by the student union (AYY) to represent
-        all students in university decisions. They bring in new ideas and ensure
-        students' voices are heard in meetings with the representative board.
-        Any Aalto student can apply to be a Halloped through an open application
-        process.
+        Hallopeds are students chosen by the student union (AYY) to represent all students in university decisions. They
+        bring in new ideas and ensure students' voices are heard in meetings with the representative board. Any Aalto
+        student can apply to be a Halloped through an open application process.
       </p>
       <p>
-        Applications are accepted through the halloped.fi portal every autumn.
-        If you're uncertain about the positions, feel free to ask. Inform the
-        Master of Studies or any board member, so that we can collaborate
-        closely within the guild.
+        Applications are accepted through the halloped.fi portal every autumn. If you're uncertain about the positions,
+        feel free to ask. Inform the Master of Studies or any board member, so that we can collaborate closely within
+        the guild.
       </p>
       <p>
-        More information about Hallopeds can be found{" "}
+        More information about Hallopeds can be found{' '}
         <a href="https://beta.halloped.fi/aalto.fi/get-started" target="_blank">
           here
         </a>
         .
       </p>
 
-      <StaticImage
-        className={style.imageLarge}
-        src="../../../images/images/hallopedihommii.png"
-        alt=""
-      />
+      <StaticImage className={style.imageLarge} src="../../../images/images/hallopedihommii.png" alt="" />
 
       <h2>Administration</h2>
       <h3>Academic Committee for Electrical Engineering</h3>
       <p>
-        The Academic Committee is one of the university's most important
-        decision-making bodies, led by the college dean. It works to improve
-        research and teaching quality by deciding on the curriculum, degree
-        requirements, and student selection criteria. Additionally, it helps
-        organize university events and awards honorary doctorates.
+        The Academic Committee is one of the university's most important decision-making bodies, led by the college
+        dean. It works to improve research and teaching quality by deciding on the curriculum, degree requirements, and
+        student selection criteria. Additionally, it helps organize university events and awards honorary doctorates.
       </p>
 
       <h3>Boards of Education</h3>
       <p>
-        Each degree program has an education council. As a member of the
-        education councils, you can
+        Each degree program has an education council. As a member of the education councils, you can
         <ul>
-          <li>
-            Influence curricula, degree requirements, and student selection
-            criteria.
-          </li>
+          <li>Influence curricula, degree requirements, and student selection criteria.</li>
           <li>Participate in the development of educational programs.</li>
           <li>Contribute to processing feedback. </li>
           <li>Appoint thesis approvers.</li>
-          <li>
-            Make decisions regarding thesis subjects, supervisors, and
-            evaluation.
-          </li>
+          <li>Make decisions regarding thesis subjects, supervisors, and evaluation.</li>
           <li>Accept evaluations of thesis work.</li>
         </ul>
-        For English-speaking AS members, the following education councils may be
-        relevant:
+        For English-speaking AS members, the following education councils may be relevant:
         <ul>
-          <li>
-            Aalto Science and Technology Bachelor Programme Committee (SCI,
-            ELEC, CHEM, ENG)
-          </li>
-          <li>
-            Degree Programme Committee of Bachelor's Programmes in Electronics,
-            Communications, and Automation
-          </li>
-          <li>
-            Degree Programme Committee for Automation and Electrical Engineering
-          </li>
+          <li>Aalto Science and Technology Bachelor Programme Committee (SCI, ELEC, CHEM, ENG)</li>
+          <li>Degree Programme Committee of Bachelor's Programmes in Electronics, Communications, and Automation</li>
+          <li>Degree Programme Committee for Automation and Electrical Engineering</li>
           <li>Degree Programme Committee for Electronics and Nanotechnology</li>
-          <li>
-            Degree Programme Committee for Computer, Communications, and
-            Information Sciences
-          </li>
+          <li>Degree Programme Committee for Computer, Communications, and Information Sciences</li>
         </ul>
       </p>
 
       <h3>Quality of Education Committee</h3>
       <p>
-        The Quality of Education Committee, or QEC, focuses on improving
-        teaching methods, setting teaching fees, and evaluating teaching
-        practices. They also organize informal discussions and training events
-        where staff can discuss current topics. QEC works closely with school
-        management, departments, learning services, and guild representatives to
-        enhance teaching. They aim to improve teaching practices in line with
-        Aalto University's goals, as well as address other important areas of
-        development. They also assess teaching skills during hiring and
-        promotions.
+        The Quality of Education Committee, or QEC, focuses on improving teaching methods, setting teaching fees, and
+        evaluating teaching practices. They also organize informal discussions and training events where staff can
+        discuss current topics. QEC works closely with school management, departments, learning services, and guild
+        representatives to enhance teaching. They aim to improve teaching practices in line with Aalto University's
+        goals, as well as address other important areas of development. They also assess teaching skills during hiring
+        and promotions.
       </p>
       <h3>Doctoral Education Council</h3>
       <p>
-        The Doctoral Education Council supervises education for graduate
-        students, often including postgraduates as student members. Its main
-        responsibilities include:
+        The Doctoral Education Council supervises education for graduate students, often including postgraduates as
+        student members. Its main responsibilities include:
         <ul>
-          <li>
-            Developing the content and structure of the university's doctoral
-            education.
-          </li>
-          <li>
-            Presenting research field proposals to the academic committee for
-            further education.
-          </li>
-          <li>
-            Making recommendations to the dean regarding the acceptance of
-            graduate students.
-          </li>
-          <li>
-            Appointing preliminary examiners for dissertation work and granting
-            permission for dissertations.
-          </li>
+          <li>Developing the content and structure of the university's doctoral education.</li>
+          <li>Presenting research field proposals to the academic committee for further education.</li>
+          <li>Making recommendations to the dean regarding the acceptance of graduate students.</li>
+          <li>Appointing preliminary examiners for dissertation work and granting permission for dissertations.</li>
           <li>Selecting opponents for dissertations and providing feedback.</li>
-          <li>
-            Appointing examiners for licentiate examinations and evaluating
-            licentiate research.
-          </li>
-          <li>
-            Handling other tasks assigned by the dean or specified in degree
-            regulations.
-          </li>
+          <li>Appointing examiners for licentiate examinations and evaluating licentiate research.</li>
+          <li>Handling other tasks assigned by the dean or specified in degree regulations.</li>
         </ul>
-        Additionally, unless specifically assigned to the chairman, the council
-        is responsible for:
+        Additionally, unless specifically assigned to the chairman, the council is responsible for:
         <ul>
-          <li>
-            Nominating responsible professors and supervisors for graduate
-            students.
-          </li>
-          <li>
-            Determining the degree language, thesis topic, and language for
-            graduate degrees.
-          </li>
+          <li>Nominating responsible professors and supervisors for graduate students.</li>
+          <li>Determining the degree language, thesis topic, and language for graduate degrees.</li>
           <li>Approving graduate students' study plans.</li>
-          <li>
-            Deciding on the timing and language of dissertation conferences and
-            appointing curators.
-          </li>
+          <li>Deciding on the timing and language of dissertation conferences and appointing curators.</li>
         </ul>
       </p>
     </PageLayout>
-  )
-}
+  );
+};
 
-export default Advocacy
+export default Advocacy;
 
-export const Head: HeadFC<HeadProps> = () => <Meta lang="en" title="Advocacy" />
+export const Head: HeadFC<HeadProps> = () => <Meta lang="en" title="Advocacy" />;
--- src/pages/en/studies/index.tsx
+++ src/pages/en/studies/index.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import type { HeadFC, HeadProps } from "gatsby"
-import Meta from "@src/components/common/Meta"
-import * as style from "./studies.module.scss"
-import PageLayout from "@src/components/layouts/PageLayout"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import type { HeadFC, HeadProps } from 'gatsby';
+import Meta from '@src/components/common/Meta';
+import * as style from './../../studies/studies.module.scss';
+import PageLayout from '@src/components/layouts/PageLayout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 interface Props extends PageProps {
-  data: ImageArrayProps
+  data: ImageArrayProps;
 }
 
 const Studies: React.FC<Props> = ({ pageContext }) => {
@@ -16,97 +16,61 @@ const Studies: React.FC<Props> = ({ pageContext }) => {
         <div className={style.list}>
           <p></p>
           <p>
-            The AS study committee represents the interests of the AS students
-            in the various institutions of the college and university. It
-            communicates on the behalf of the students with these institutions,
-            as well as directly with the school staff. If you are interested in
-            discussing any peculiarities relating to your studies or want to
-            give feedback, you can contact the opintomestari
-            (opintomestari@as.fi).
+            The AS study committee represents the interests of the AS students in the various institutions of the
+            college and university. It communicates on the behalf of the students with these institutions, as well as
+            directly with the school staff. If you are interested in discussing any peculiarities relating to your
+            studies or want to give feedback, you can contact the opintomestari (opintomestari@as.fi).
           </p>
           <h2>Exam Archive (Tenttiarkisto)</h2>
           <div>
             <a href="https://tenttiarkisto.fi" target="_blank">
               Tenttiarkisto
-            </a>{" "}
-            is a student run website where you can find exam papers for previous
-            instances of various courses. Students independently scan and public
-            exams from different courses onto the website. It is a highly useful
-            website for studying for exams you may have coming up.
+            </a>{' '}
+            is a student run website where you can find exam papers for previous instances of various courses. Students
+            independently scan and public exams from different courses onto the website. It is a highly useful website
+            for studying for exams you may have coming up.
           </div>
 
           <h2>FAQs</h2>
-          <h3>
-            1. I'm unable to attend a compulsory exam/lecture for a course, what
-            should I do?
-          </h3>
+          <h3>1. I'm unable to attend a compulsory exam/lecture for a course, what should I do?</h3>
           <p>
-            Contact your course's teachers and assistants as soon as possible.
-            Their details are generally written on the Sisu course page and
-            MyCourses. You can get alternate arrangements for course grading,
-            through retake exams or extra assignments. Furthermore, many
-            exam-based courses have retakes you can register for through Sisu.
+            Contact your course's teachers and assistants as soon as possible. Their details are generally written on
+            the Sisu course page and MyCourses. You can get alternate arrangements for course grading, through retake
+            exams or extra assignments. Furthermore, many exam-based courses have retakes you can register for through
+            Sisu.
           </p>
 
           <h3>
-            2. I didn't like the structure or teaching methods of a course, or
-            want to help improve its structure.
+            2. I didn't like the structure or teaching methods of a course, or want to help improve its structure.
           </h3>
           <p>
-            Teaching staff publish forms at the end of a course to understand
-            and incorporate student feedback for future semesters. We can also
-            provide feedback to relevant staff. You can also contact the study
-            tutor (opintomestari@as.fi) or use the telegram bot
-            (@ASFeedbackStudiesBot).
+            Teaching staff publish forms at the end of a course to understand and incorporate student feedback for
+            future semesters. We can also provide feedback to relevant staff. You can also contact the study tutor
+            (opintomestari@as.fi) or use the telegram bot (@ASFeedbackStudiesBot).
+          </p>
+
+          <h3>3. I wish to go on an exchange semester. What should I do / Whom should I contact?</h3>
+          <p>
+            Exchange semester registration typically occurs in January. Some slots can also be selected in the September
+            application period. You can start your research from the website for exchange studies on
+            <a href={'https://www.aalto.fi/en/other-studies/exchange-studies'}>aalto.fi</a>
+            The preparation of an exchange semester is primarily done by a student, and therefore you will have to find
+            a suitable destination, programme, and appropriate courses. We highly recommend contacting your relevant
+            counselors for exchange semester guidance, ensuring you take care of ideal destinations, study plans,
+            exchange grants, and so on! You can also refer to experiences and information from previous students through
+            the Aalto travel reports <a href={'https://wiki.aalto.fi/pages/viewpage.action?pageId=130485578'}>wiki</a>.
           </p>
 
           <h3>
-            3. I wish to go on an exchange semester. What should I do / Whom
-            should I contact?
+            4. I am struggling with certain aspects of my studies or want to take a break from them. Where can I get
+            help?
           </h3>
           <p>
-            Exchange semester registration typically occurs in January. Some
-            slots can also be selected in the September application period. You
-            can start your research from the website for exchange studies on
-            <a href={"https://www.aalto.fi/en/other-studies/exchange-studies"}>
-              aalto.fi
-            </a>
-            The preparation of an exchange semester is primarily done by a
-            student, and therefore you will have to find a suitable destination,
-            programme, and appropriate courses. We highly recommend contacting
-            your relevant counselors for exchange semester guidance, ensuring
-            you take care of ideal destinations, study plans, exchange grants,
-            and so on! You can also refer to experiences and information from
-            previous students through the Aalto travel reports{" "}
-            <a
-              href={
-                "https://wiki.aalto.fi/pages/viewpage.action?pageId=130485578"
-              }
-            >
-              wiki
-            </a>
-            .
-          </p>
-
-          <h3>
-            4. I am struggling with certain aspects of my studies or want to
-            take a break from them. Where can I get help?
-          </h3>
-          <p>
-            If you are having issues with your study plan structure or want
-            guidance on how to move forward with your degree, contact your study
-            counselors through{" "}
-            <a href={"https://mystudies.aalto.fi/s/"}>MyStudies</a>. You may
-            also ask for help in career planning and stress management through
-            study psychologists. This relevant support can be found through
-            Aalto's{" "}
-            <a
-              href={
-                "https://www.aalto.fi/en/services/where-to-get-guidance-and-support"
-              }
-            >
-              support services
-            </a>
+            If you are having issues with your study plan structure or want guidance on how to move forward with your
+            degree, contact your study counselors through <a href={'https://mystudies.aalto.fi/s/'}>MyStudies</a>. You
+            may also ask for help in career planning and stress management through study psychologists. This relevant
+            support can be found through Aalto's{' '}
+            <a href={'https://www.aalto.fi/en/services/where-to-get-guidance-and-support'}>support services</a>
           </p>
         </div>
 
@@ -115,18 +79,12 @@ const Studies: React.FC<Props> = ({ pageContext }) => {
           <div className={style.sideList}>
             General:
             <li>
-              <a
-                href="https://www.aalto.fi/en/student-guide/academic-calendar"
-                target="_blank"
-              >
+              <a href="https://www.aalto.fi/en/student-guide/academic-calendar" target="_blank">
                 The University's Academic Calendar
               </a>
             </li>
             <li>
-              <a
-                href="https://www.aalto.fi/en/school-of-electrical-engineering"
-                target="_blank"
-              >
+              <a href="https://www.aalto.fi/en/school-of-electrical-engineering" target="_blank">
                 ELEC Website
               </a>
             </li>
@@ -178,28 +136,19 @@ const Studies: React.FC<Props> = ({ pageContext }) => {
               </a>
             </li>
             <li>
-              <a
-                href="https://www.aalto.fi/en/other-studies/summer-studies"
-                target="_blank"
-              >
+              <a href="https://www.aalto.fi/en/other-studies/summer-studies" target="_blank">
                 Summer Courses
               </a>
             </li>
             <div className={style.sideList}>
               Exchange Studies:
               <li>
-                <a
-                  href="https://www.aalto.fi/en/other-studies/exchange-studies"
-                  target="_blank"
-                >
+                <a href="https://www.aalto.fi/en/other-studies/exchange-studies" target="_blank">
                   Aalto's official guide on exchange studies
                 </a>
               </li>
               <li>
-                <a
-                  href="https://www.ayy.fi/en/apply-for-financial-support/scholarships"
-                  target="_blank"
-                >
+                <a href="https://www.ayy.fi/en/apply-for-financial-support/scholarships" target="_blank">
                   The AYY scholarship stipend for exchange studies
                 </a>
               </li>
@@ -207,37 +156,25 @@ const Studies: React.FC<Props> = ({ pageContext }) => {
             <div className={style.sideList}>
               Guidance and Support:
               <li>
-                <a href={"https://www.nyyti.fi/en/"}>
-                  Nyyti.fi (mental health organization for students)
-                </a>
+                <a href={'https://www.nyyti.fi/en/'}>Nyyti.fi (mental health organization for students)</a>
               </li>
               <li>
-                <a
-                  href={
-                    "https://www.aalto.fi/en/services/where-to-get-guidance-and-support"
-                  }
-                >
+                <a href={'https://www.aalto.fi/en/services/where-to-get-guidance-and-support'}>
                   Aalto's official guide on getting guidance and support
                 </a>
               </li>
               <li>
-                <a href={"https://mystudies.aalto.fi/s/"}>
-                  MyStudies (Aalto study guidance counsellors)
-                </a>
+                <a href={'https://mystudies.aalto.fi/s/'}>MyStudies (Aalto study guidance counsellors)</a>
               </li>
             </div>
           </div>
-          <StaticImage
-            className="tall"
-            src="../../../images/images/labrahommii.png"
-            alt=""
-          />
+          <StaticImage className="tall" src="../../../images/images/labrahommii.png" alt="" />
         </div>
       </div>
     </PageLayout>
-  )
-}
+  );
+};
 
-export default Studies
+export default Studies;
 
-export const Head: HeadFC<HeadProps> = () => <Meta lang="en" title="Studies" />
+export const Head: HeadFC<HeadProps> = () => <Meta lang="en" title="Studies" />;
